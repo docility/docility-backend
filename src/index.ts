821219e1a18d3812bf3b07940e82f9863c8fcfad
Rene Boy Andoy <reneboyandoy@Renes-MacBook-Air.local>
2025-04-09 19:22:29 +0800

    register socket

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import type { Core } from '@strapi/strapi';
+import type { Core } from '@strapi/strapi';
 const { Server } = require('socket.io');
 export default {
   /**
@@ -7,7 +7,40 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
+  register({ strapi }: { strapi: Core.Strapi }) {
+
+    const io = new Server(strapi.server.httpServer, {
+      cors: {
+        origin: "*", // Replace with your frontend URL(s) for security
+        methods: ["GET", "POST"],
+      },
+    });
+
+    // Store connected clients with their IP as the key
+    const clients = new Map();
+
+    io.on('connection', (socket) => {
+      // Extract client IP address
+      const userId = socket.handshake.auth.userId;
+      console.log(socket.handshake)
+      // const normalizedIp = userId.split(':').pop(); // Normalize IP (remove IPv6 prefix)
+
+      console.log(`New client connected from IP: ${userId}`);
+
+      // Associate the IP with the socket ID
+      clients.set(userId, socket.id);
+      console.log('Clients:', clients);
+
+      // Handle disconnection
+      socket.on('disconnect', () => {
+        console.log(`Client disconnected from IP: ${userId}`);
+        clients.delete(userId); // Remove the client from the map
+      });
+    });
+
+    strapi.io = io; // Attach Socket.IO instance to Strapi
+    strapi.io.clients = clients; // Attach the clients map to strapi.io
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
